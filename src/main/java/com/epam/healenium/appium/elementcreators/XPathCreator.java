fc5b2a2e40473f321b2b61f2845a40ab1352caa5
Anna Chernyshova <anna.chernyshova91@gmail.com>
2019-12-23 22:52:44 +0300

    updated dependencies

--- src/main/java/com/epam/healenium/appium/elementcreators/XPathCreator.java
+++ src/main/java/com/epam/healenium/appium/elementcreators/XPathCreator.java
@@ -13,7 +13,7 @@
 package com.epam.healenium.appium.elementcreators;
 
 import com.epam.healenium.engine.elementcreators.ElementCreator;
-import com.epam.sha.treecomparing.Node;
+import com.epam.healenium.treecomparing.Node;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.stream.Collectors;
